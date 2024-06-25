7995b8377a55f8b31a9f39e85f25e7a8b64edbaa
Vaisakh Murali <vaisakhmurali@gmail.com>
2024-06-25 11:59:42 +0530

    sm8150-common: Use vendor/hardware/xiaomi
    Change-Id: I0d61c191f2cace28b30921c1d630a026bcab1f7f

--- msmnile.mk
+++ msmnile.mk
@@ -453,7 +453,7 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/xiaomi
+    vendor/hardware/xiaomi
 
 # Telephony
 PRODUCT_PACKAGES += \
